59e549481de38c204fcbb3d0d3a50050af0e49a1
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-12 04:53:33 +0800

    chore: setup tailwind, shadcn, and tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,28 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import wails from "@wailsio/runtime/plugins/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
   server: {
     host: "127.0.0.1",
     port: Number(process.env.WAILS_VITE_PORT) || 9245,
     strictPort: true,
   },
-  plugins: [react(), wails("./bindings")],
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    wails("./bindings"),
+  ],
 });
